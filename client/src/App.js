e56ae2e5337e7afec002b57c60edf46ca0b82dd0
Sebastian Urbina <sebita.urbina@gmail.com>
2020-11-11 23:31:57 -0300

    feat: add drizzle

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,40 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom';
+import { drizzleReactHooks } from '@drizzle/react-plugin';
+import { Drizzle } from '@drizzle/store';
 
 import Admin from './Admin';
 import Match from './Match';
 import MatchList from './MatchList';
+import Prode from './contracts/Prode.json';
+
+const { DrizzleProvider } = drizzleReactHooks;
+
+const drizzleOptions = {
+  contracts: [Prode],
+};
+const drizzle = new Drizzle(drizzleOptions);
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/admin">
-          <Admin />
-        </Route>
-        <Route path="/matches" exact>
-          <MatchList />
-        </Route>
-        <Route path="/matches/:id">
-          <Match />
-        </Route>
-        <Route path="/">
-          <Redirect to="/matches" />
-        </Route>
-      </Switch>
+      <DrizzleProvider drizzle={drizzle}>
+        <Switch>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/matches" exact>
+            <MatchList />
+          </Route>
+          <Route path="/matches/:id">
+            <Match />
+          </Route>
+          <Route path="/">
+            <Redirect to="/matches" />
+          </Route>
+        </Switch>
+      </DrizzleProvider>
     </Router>
   );
 }
