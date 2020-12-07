cc75897d671428910117bf7e89807d6408dd0e75
Sebastian Urbina <sebita.urbina@gmail.com>
2020-12-07 16:12:42 -0300

    feat: improve Match contract

--- contracts/Match.sol
+++ contracts/Match.sol
@@ -12,20 +12,27 @@ contract Match is Initializable, Ownable {
   }
 
   struct Bet {
-    address payable author;
     bool payed;
     MatchResult result;
+    uint listPointer;
   }
+  mapping (address => Bet) betsMap;
+  address[] gamblers;
 
   string public homeTeamId;
   string public awayTeamId;
   uint public matchStartDate; // Unix timestamp depicting start date
 
-  Bet[] bets; // Using a list allows for a user to make multiple bets
   uint public jackpot = 0;
+  uint public prizeAmount = 0; // Calculated after the final result is know, hold the amount of ether each winner will get
 
   // TODO: Temporaly store match result
   MatchResult public finalResult;
+
+  /////////////////////
+  //     Modifiers    //
+  /////////////////////
+
 
   modifier betsAreOpen() {
     // Users can place bet up to one hour before the match starts
@@ -41,7 +48,10 @@ contract Match is Initializable, Ownable {
     // Users can claim bets 4 hours after the match hast started
     // This is because the time a match ends is not know in advance and because
     // When creating a block a node can select as timestamp a value that's up to 15 minutes in the future
-    require(block.timestamp > (matchStartDate + 4 hours));
+    require(
+      block.timestamp > (matchStartDate + 4 hours),
+      'Bet cannot be collected yet'
+    );
     require(
       finalResult.homeTeamScore > -1 && finalResult.awayTeamScore > -1,
       'Final result has not been loaded yet'
@@ -49,6 +59,17 @@ contract Match is Initializable, Ownable {
     _;
   }
 
+  modifier userHasPlacedBet() {
+    // Validates a user has actually placed a bet for this match
+    require(
+      hasPlacedBet(msg.sender),
+      'User has not placed any bet'
+    );
+    _;
+  }
+
+  // initialize function replaces constructor
+  // check the Initializable contract for further information
   function initialize(
     string memory _homeTeamId,
     string memory _awayTeamId,
@@ -67,6 +88,11 @@ contract Match is Initializable, Ownable {
     });
   }
 
+  /////////////////////
+  //  User functions //
+  /////////////////////
+
+
   function placeBet(
     int16 homeTeamScore,
     int16 awayTeamScore
@@ -76,55 +102,65 @@ contract Match is Initializable, Ownable {
       'Score must be greater than or equal to 0'
     );
 
-    bets.push(Bet({
-      result: MatchResult({
+    if(hasPlacedBet(msg.sender)) {
+      // If the user already placed a bet, override current result
+      betsMap[msg.sender].result = MatchResult({
         homeTeamScore: homeTeamScore,
         awayTeamScore: awayTeamScore
-      }),
-      author: msg.sender,
-      payed: false
-    }));
+      });
+    } else {
+      // If the users has not placed a bet, create and store a new bet
+      gamblers.push(msg.sender);
+      uint index = gamblers.length - 1;
+
+      betsMap[msg.sender] = Bet({
+        payed: false,
+        result: MatchResult({
+          homeTeamScore: homeTeamScore,
+          awayTeamScore: awayTeamScore
+        }),
+        listPointer: index
+      });
+    }
 
     jackpot = jackpot + msg.value;
   }
 
-  function claimBet() public canCollectPrizes returns(bool) {
-    uint selectedBetIndex = 0;
-    bool found = false;
+  function hasPlacedBet(address user) public view returns(bool isIndeed) {
+    if(gamblers.length == 0) return false;
+    return (gamblers[betsMap[user].listPointer] == user);
+  }
 
-    for(uint i = 0; i < bets.length; i++) {
-      // Find bet
-      // - author === to sender
-      // - bet result === finalResult
-      // - not payed
-      Bet storage currentBet = bets[i];
-      if (
-        currentBet.author == msg.sender &&
-        currentBet.result.homeTeamScore == finalResult.homeTeamScore &&
-        currentBet.result.awayTeamScore == finalResult.awayTeamScore &&
-        !currentBet.payed
-      ) {
-        found = true;
-        selectedBetIndex = i;
-        break;
-      }
-    }
+  function isBetSuccessful(
+    int16 homeTeamScore,
+    int16 awayTeamScore
+  ) public view canCollectPrizes returns(bool isIndeed) {
+    return finalResult.homeTeamScore == homeTeamScore && finalResult.awayTeamScore == awayTeamScore;
+  }
 
-    // If a bet was found
-    if(found) {
-      Bet storage selectedBet = bets[selectedBetIndex];
+  function claimBet() public canCollectPrizes userHasPlacedBet returns(bool) {
+    Bet memory bet = betsMap[msg.sender];
 
+    // If bet has not been paid yet and is successful
+    if(
+      !bet.payed &&
+      isBetSuccessful(bet.result.homeTeamScore, bet.result.awayTeamScore)
+    ) {
       // Mark bet as payed
-      selectedBet.payed = true;
-
-      if(!msg.sender.send(jackpot)) {
-        selectedBet.payed = false;
+      bet.payed = true;
+      // Attemp sending money
+      if(!msg.sender.send(prizeAmount)) {
+        bet.payed = false;
         return false;
       }
     }
 
     return true;
   }
+
+  /////////////////////
+  // Admin functions //
+  /////////////////////
 
   function setFinalResult(
     int16 homeTeamScore,
@@ -139,5 +175,22 @@ contract Match is Initializable, Ownable {
       homeTeamScore: homeTeamScore,
       awayTeamScore: awayTeamScore
     });
+
+    // Calculate amount to pay
+    uint winnersCount = 0;
+
+    for(uint i = 0; i < gamblers.length; i++) {
+      // Count all the bets that were winning bets
+      Bet memory currentBet = betsMap[gamblers[i]];
+      if (isBetSuccessful(currentBet.result.homeTeamScore, currentBet.result.awayTeamScore)) {
+        winnersCount = winnersCount + 1;
+      }
+    }
+
+    // Validate there is at least one winner
+    if(winnersCount > 0) {
+      // uint division will round down the result
+      prizeAmount = jackpot / winnersCount;
+    }
   }
 }
